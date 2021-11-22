b9d807bd68fbcce923e3a0e64f2b00bc57a6e64f
khalilhamdii <khaalil.hamdi@gmail.com>
2021-11-22 23:40:48 +0100

    Add Annonces container component

--- app/javascript/components/Home.js
+++ app/javascript/components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Header';
+import Annonces from '../containers/Annonces';
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <Annonces />
     </div>
   );
 };
---
+++ app/javascript/containers/Annonces.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import AnnonceCard from '../components/AnnonceCard';
+
+const Annonces = () => {
+  return (
+    <div className="py-5">
+      <div className="container px-4 px-lg-5 mt-5">
+        <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+          <AnnonceCard />
+          <AnnonceCard />
+          <AnnonceCard />
+          <AnnonceCard />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Annonces;
